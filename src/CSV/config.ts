8a713b452a1487610e69c28b1d0ca906d97e6fcc
harish18092002 <harish18092002@gmail.com>
2024-05-16 13:05:42 +0530

    CSV to JSON

---
+++ src/CSV/config.ts
@@ -0,0 +1,18 @@
+import { ServiceAccount, cert } from 'firebase-admin/app';
+
+export const firebaseCredentials = {
+    type: 'service_account',
+    project_id: 'nestjs-parsecsv',
+    private_key_id: 'fcef731c0e0241ffd81b3dec8dffbed4f16ad3d5',
+    private_key:
+        '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC2XGefeSGpSRdf\nPsYPd5FHCZ+kstnO4vgjjQWNvC39mHbuHfZQ6nc5VOxv+5icq5qdkdu+3OnCRSVH\nTuGlosC9Dsip8kr/H8R1GA03zAJWosaQLuuzylYxuOsairpTGNXeIDsyjSU2FomS\nWhCYVE+Y15AiViR1jKeD9lWRD0iw8XIhm4FJ3of8uy4e6cvDeVD4YIgB6I2wz6I7\nDkstOyO5DWDVeEfdOu4MWNh/82eG/N37UWRjxRV53awLwkkEzyOF8hYoHgmjp6iL\nUu5xwvHgbDPmcs1iUmwQWH4IxXczO2dacN5CLompDQKmRm67URl9SJnxWHRAVdMY\nwQhp6SJBAgMBAAECggEAAlwPuzCGDvqzIrhdN7YEWzb0qdzmlJa7PzqD5+GXdsCS\nSWJ53XIrOMKwjIJk5yIRThilD0MsgycB5HpQ0SPZ+phGYGySK9rtX9Z/1Uj+fsB8\ncUEqK5lipCIQaLeC0bBwk/82xOCuqpSAEzS3IkF1BjVD2C3lKuA3Cq4VTVs1RZTU\njPTcrbIcyihKxBNaQ0Ia2/6jZsTzGLybG4l1num/zAc128HQAhDNaUnPYkNQAAgz\nAkxRN8y1pG/hxpyeOQhZPMUcwJAcJu0OeRq+075g94k/wfiun3zNHLlPF361L4p2\ngxrvUoYcrcZ2Ni5LPnYLWDpMRNWCRLepPWWJAgIfAQKBgQD72D/idrXgZzvU5Fbi\nCyq6o8xRxvet/q7rN1sDPfgEkB/TDggDDIbS5Im2cr0IbszyowXnl7ELzv49U3yg\nJq7DkC8yk2LvK2w3lNlxoPzKLzVrbcc2SxG4f54CGlHWve5i2j6N9YEjJbVUyCxv\nMu5gmnIUtBVvQFW9utLp9NdEBQKBgQC5XqrTL0whdvtyee8wLVP8Voq719DqDf2+\nN3QjmIJNt7faKo6HRGImmY24+RPgf/neg/hQFSgheOc3oJskvylZlafdCZpIpcEO\nf8RgSl9MjFGP87USNZ2AY1F7sFLRSe9qqzs9vvYYDnhJcnor++vWY4y1zaF6quO4\nEQRWJG1WDQKBgADwCSzarEqtl7dvMjOdPwd63KPhTcLEE/qkNh6I+Idr0fKDNtxs\nC4R1fV3Eh5ekcfblTHxUQEB/AMtRqEEgY74upz5U9iVV0A1U7IpTwx0tl9FSk09M\ncEBzmS8A0hUul9ieX2U/6L8swMQUCrZPRjvt10bkdL/5SVhvfaGJW9PRAoGBAJTk\nzm+9QPhsWsJz4MUg06IPXXZOfrdRr6w3XKku2hNU629o2X0bV39LvcODBg1AnlK2\nTCD/LXHdbFOJRLS6e5tCk0f/a5bI1Q4xYGo7stqIpwbqqVnMCQvUOuIQGiWTrqRO\nj9Ij7gYasVHFiw6DVsaoxaBOYFNoqmrx+Dq9HbmNAoGBAPZXwDIKdzQdlq10yW/0\npmM4yDGnehnJH6sQ59PatmTVmvKS8tlvQZzNSYrLB3wraT7+5D6MqxSnBtb0d+1E\nCNfGJkFQmIQVcHWFdSWoorUfCkSAfe2eneACaokAcwokvhqAXO8ngK0NGPOO7fZK\ntP/O1qH52x2/zNmbiPa+VAfR\n-----END PRIVATE KEY-----\n',
+    client_email:
+        'firebase-adminsdk-wdqvg@nestjs-parsecsv.iam.gserviceaccount.com',
+    client_id: '105409230941904220276',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+        'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wdqvg%40nestjs-parsecsv.iam.gserviceaccount.com',
+    universe_domain: 'googleapis.com',
+};
